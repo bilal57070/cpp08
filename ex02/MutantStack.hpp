9674a10522da0e462b48e96f6adba7cd980d0103
Bilal Safi <bsafi@c2r9p1.42nice.fr>
2025-01-15 19:12:59 +0100

    '

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,19 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+
+
+template<typename T> class MutantStack{
+    public:
+        MutantStack();
+        ~MutantStack();
+};
+
+
+
+
+
+
+
+
+#endif
