527dc9b6608df8ed38ca5657e5c11dce35c9d89e
Bilal Safi <bsafi@c1r8p1.42nice.fr>
2025-01-18 14:42:48 +0100

    '

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,27 +3,52 @@
 
 # include <iostream>
 # include <algorithm>
+# include <stack>
 
-template<typename T> class MutantStack : public std::stack{
+template<typename T> class MutantStack : public std::stack<T> {
     public:
         MutantStack();
         ~MutantStack();
         MutantStack(MutantStack const &cp);
         MutantStack& operator=(MutantStack const &cpi);
 
-        const MutantStack& top();
-        bool empty() const;
-        size_t size() const;
-        void push(const T& value);
-        void pop();
-        T::iterator begin();
-        //void print();
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 
+        iterator begin(){
+            return this->c.begin();
+        }
 
-    private:
-        T *tab;
-        size_t _size;
+        const_iterator begin() const {
+            return this->c.begin();
+        }
+
+        iterator end(){
+            return this->c.end();
+        }
+
+        const_iterator end() const {
+            return this->c.end();
+        }
+
+        reverse_iterator rbegin(){
+            return this->c.rbegin();
+        }
+
+        reverse_iterator rend(){
+            return this->rend();
+        }
+
+        const_reverse_iterator rbegin() const {
+            return this->c.rbegin();
+        }
+
+        const_reverse_iterator rend() const {
+            return this->c.rend();
+        }
 };
 
 
--- ex02/MutantStack.tpp
+++ ex02/MutantStack.tpp
@@ -1,70 +1,20 @@
 #include "MutantStack.hpp"
 
 template<typename T>
-MutantStack<T>::MutantStack(){
-    tab = new T[0];
-    _size = 0;
+MutantStack<T>::MutantStack() : std::stack<T>(){
 }
 
 template<typename T>
 MutantStack<T>::~MutantStack(){}
 
 template<typename T>
-MutantStack<T>::MutantStack(MutantStack const &cp){
+MutantStack<T>::MutantStack(MutantStack const &cp) : std::stack<T>(cp){
     *this = cp;
 }
 
 template<typename T>
 MutantStack<T>& MutantStack<T>::operator=(MutantStack const &cpi){
-    _size = cpi._size;
-    tab = new T(cpi._size);
-    for (int i = 0; i < _size; i++)
-        tab[i] = cpi.tab[i];
+    std::stack<T>::operator=(cpi);
     return *this;
 }
 
-template<typename T>
-size_t  MutantStack<T>::size() const{
-    return (_size);
-}
-
-template<typename T>
-const MutantStack<T>&   MutantStack<T>::top(){
-    return (tab[_size]);
-}
-
-template<typename T>
-bool    MutantStack<T>::empty() const{
-    if (_size == 0)
-        return true;
-    else
-        return false;
-}
-
-template<typename T>
-void    MutantStack<T>::pop(){
-    delete tab;
-    tab = new T[0];
-    _size = 0;
-}
-
-template<typename T>
-void    MutantStack<T>::push(const T& v){
-    T *newtab = new T[_size + 1];
-
-    for (size_t i = 0; i < _size; i++){
-        newtab[i] = tab[i];
-    }
-    //std::cout << _size << " la taille en gros" << std::endl;
-    newtab[_size] = v;
-    _size++;
-    delete tab;
-    tab = newtab; 
-}
-
-/*template<typename T>
-void    MutantStack<T>::print(){
-    for (size_t i = 0; i < _size; i++){
-        std::cout << tab[i] << std::endl;
-    }
-}*/
